fc89ae64e505a4060d77c27cabe423b484f11f88
Philippe Collet <Philippe.Collet@unice.fr>
2021-08-22 17:45:53 +0200

    configure failsafe plugin in maven with 1 IT test

--- backend/src/test/java/fr/univcotedazur/simpletcfs/controllers/CustomerCareWebAutoConfigureIT.java
+++ backend/src/test/java/fr/univcotedazur/simpletcfs/controllers/CustomerCareWebAutoConfigureIT.java
@@ -1,47 +1,37 @@
 package fr.univcotedazur.simpletcfs.controllers;
 
-import fr.univcotedazur.simpletcfs.CatalogExplorator;
-import fr.univcotedazur.simpletcfs.entities.Cookies;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Set;
 
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CustomerCareController.class) // start only the specified MVC front controller and no other Spring components nor the server -> Unit test of the controller
-public class CustomerCareWebMvcTest {
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc // start the FULL Web+Back stack (all controllers + all components) but not the server -> Integration test
+public class CustomerCareWebAutoConfigureIT {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private CatalogExplorator mockedCat; // the real Catalog component is not created, we have to mock it
-
     @Test
     void recipesFullStackTest() throws Exception {
-        when(mockedCat.listPreMadeRecipes())
-                .thenReturn(Set.of(Cookies.CHOCOLALALA,Cookies.DARK_TEMPTATION)); // only 2 of the 3 enum values
-
         mockMvc.perform(get("/customer/recipes"))
                     .andDo(print())
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$").isArray())
-                    .andExpect(jsonPath("$", hasSize(2)))
+                    .andExpect(jsonPath("$", hasSize(3)))
                     .andExpect(jsonPath("$", hasItem("CHOCOLALALA")))
-                    .andExpect(jsonPath("$", hasItem("DARK_TEMPTATION")));
+                    .andExpect(jsonPath("$", hasItem("DARK_TEMPTATION")))
+                    .andExpect(jsonPath("$", hasItem("SOO_CHOCOLATE")));
         }
 
 }
