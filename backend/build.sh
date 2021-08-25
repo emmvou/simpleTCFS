12d4c8d25aea35bdbd186a9b450ccb20e2b30864
Philippe Collet <Philippe.Collet@unice.fr>
2021-08-25 15:57:00 +0200

    dockerized backend (as a jar)

---
+++ backend/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Preparing environment
+echo "Compiling the TCF Spring backend"
+mvn -q -DskipTests clean package
+echo "Done"
+
+# building the docker image
+docker build --build-arg JAR_FILE=target/simpleTCFS-0.0.1-SNAPSHOT.jar -t pcollet/tcf-spring-backend .
