a89c195b6698465233b4277a4b16e1cfdccc4bb2
Philippe Collet <Philippe.Collet@unice.fr>
2021-08-25 20:33:24 +0200

    graceful exit of the springapplication cli

--- cli/src/main/java/fr/univcotedazur/simpletcfs/cli/CliApplication.java
+++ cli/src/main/java/fr/univcotedazur/simpletcfs/cli/CliApplication.java
@@ -16,8 +16,9 @@ public class CliApplication {
     private String serverHostandPort;
 
     public static void main(String[] args) {
-        SpringApplication.run(CliApplication.class, args);
+        SpringApplication.exit(SpringApplication.run(CliApplication.class, args), () -> 0);
     }
+
 
     /* Spring docs recommend to use the non-blocking, reactive WebClient which offers efficient support for both sync,
    async and streaming scenarios. RestTemplate will be deprecated in the future versions. -> still, MVP mode here
