6442280dd83990157fa385197acc617fa15cc73c
Nassim Bounouas <nassim.bounouas@univ-cotedazur.fr>
2022-01-28 20:04:56 +0100

    wait-for-it.sh, docker-compose simplified, dockerfile update
    wait-for-it.sh script moved into cli folder (and image). Docker-compose
    simplified to use default network and override cli entrypoint. Dockerfile updated (label and
    wait-for-it)

--- cli/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Running the image as
-#  - removing the container after exit (ease housekeeping because it's a POC)
-#  -it interactive shell
-# passing the URL of the TCF server as an environment variable (see Dockerfile)
-docker run --env SERVER_URL=$1 --rm -it pcollet/tcf-spring-cli
-
-# ^C to stop
