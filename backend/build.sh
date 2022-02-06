97df7b9ce60c5d59dac6271323b317320333fd10
Emmeline Vouriot <emm.vou@gmail.com>
2022-02-06 14:08:25 +0100

    update check

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,5 @@ echo "Compiling the TCF Spring backend"
 mvn clean package
 echo "Done"
 
-# building the docker image
+# building the docker  image
 docker build --build-arg JAR_FILE=target/simpleTCFS-0.0.1-SNAPSHOT.jar -t pcollet/tcf-spring-backend .
