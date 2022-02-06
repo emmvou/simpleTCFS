2804e79e0cc734e8f12ab9c08fdf679b167b58f6
Emmeline Vouriot <emm.vou@gmail.com>
2022-02-06 14:02:47 +0100

    update

--- cli/build.sh
+++ cli/build.sh
@@ -5,5 +5,5 @@ echo "Compiling the TCF Spring CLI"
 mvn clean package
 echo "Done"
 
-# building the docker  image
+# building the docker image
 docker build --build-arg JAR_FILE=target/cli-0.0.1-SNAPSHOT.jar -t pcollet/tcf-spring-cli .
